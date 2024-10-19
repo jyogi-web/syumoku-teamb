74be4218c5f7e394569db7624e3e6ac2f9c6b035
pansyu <gutti1225yokono@gmail.com>
2024-10-19 17:00:44 +0900

    w分けわかめ

---
+++ Assets/Script/Money.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Money : MonoBehaviour
+{
+    public Text balanceText; //残高を表示するUI
+    private int balance = 1000; //残高の値
+    void Update()
+    {
+        UpdateBalanceText();
+    }
+
+    void UpdateBalanceText()
+    {
+        balanceText.text = "残高: " + balance.ToString() + "円";
+    }
+
+    public void SetBalance(int newBalance)
+    {
+        balance = newBalance;
+        UpdateBalanceText();
+    }
+}
