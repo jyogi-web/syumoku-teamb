4e2b07ba8d733bb49ecdd95f0c8427e70b45d0e4
wasijyayo <meganekaitai@gmail.com>
2024-10-20 12:39:24 +0900

    fusionシーン仮置きしました。

--- Assets/Script/firebird.cs
+++ Assets/Script/firebird.cs
@@ -21,7 +21,7 @@ public class firebird : MonoBehaviour
         {9,1,2,3,120,0},
         {10,1,2,3,120,0}
     };
-    //sozai_hairetu
+    //素材配列
     public static int[,] parts = new int[20, 3]
     {
         {1,30,0},
@@ -45,7 +45,7 @@ public class firebird : MonoBehaviour
         {19,30,0},
         {20,30,0}
     };
-    //nanbaringu
+    //ナンバリング
     public static string[] parts_name = new string[20]
     {
         "もも肉",
@@ -69,104 +69,115 @@ public class firebird : MonoBehaviour
         "手羽先",
         "つくね"
     };
+    //合成スロット処理
 
+    //選択しているスロットの管理
+    public static int[] chose_srotto = new int[]{0, 0, 0};
     //ボタン押した際の処理
-    public void momoparts()
+    void momoparts()
     {
-        parts[0, 2] += 1;
-        money -= parts[0, 1];
+        //if(fusion==true)
+        //{
+
+        //}
+        //else
+        //{
+            //parts[0, 2] += 1;
+            //money -= parts[0, 1];
+        //}
+        
     }
-    public void eggparts()
+    void eggparts()
     {
         parts[1, 2] += 1;
         money -= parts[1, 1];
     }
-    public void bonjiri_parts()
+    void bonjiri_parts()
     {
         parts[2, 2] += 1;
         money -= parts[2, 1];
     }
-    public void butabarag_parts()
+    void butabarag_parts()
     {
         parts[3, 2] += 1;
         money -= parts[3, 1];
     }
-    public void ginnan_parts()
+    void ginnan_parts()
     {
         parts[4, 2] += 1;
         money -= parts[4, 1];
     }
-    public void hatu_parts()
+    void hatu_parts()
     {
         parts[5, 2] += 1;
         money -= parts[5, 1];
     }
-    public void furisode_parts()
+    void furisode_parts()
     {
         parts[6, 2] += 1;
         money -= parts[6, 1];
     }
-    public void kasira_parts()
+    void kasira_parts()
     {
         parts[7, 2] += 1;
         money -= parts[7, 1];
     }
-    public void kawa_parts()
+    void kawa_parts()
     {
         parts[8, 2] += 1;
         money -= parts[8, 1];
     }
-    public void mune_parts()
+    void mune_parts()
     {
         parts[9, 2] += 1;
         money -= parts[9, 1];
     }
-    public void nankotu_parts()
+    void nankotu_parts()
     {
         parts[10, 2] += 1;
         money -= parts[10, 1];
     }
-    public void nwgi_parts()
+    void nwgi_parts()
     {
         parts[11, 2] += 1;
         money -= parts[11, 1];
     }
-    public void reba_parts()
+    void reba_parts()
     {
         parts[12, 2] += 1;
         money -= parts[12, 1];
     }
-    public void sasami_parts()
+    void sasami_parts()
     {
         parts[13, 2] += 1;
         money -= parts[13, 1];
     }
-    public void seseri_parts()
+    void seseri_parts()
     {
         parts[14, 2] += 1;
         money -= parts[14, 1];
     }
-    public void siitake_parts()
+    void siitake_parts()
     {
         parts[15, 2] += 1;
         money -= parts[15, 1];
     }
-    public void sunazuri_parts()
+    void sunazuri_parts()
     {
         parts[16, 2] += 1;
         money -= parts[16, 1];
     }
-    public void tann_parts()
+    void tann_parts()
     {
         parts[17, 2] += 1;
         money -= parts[17, 1];
     }
-    public void tebasaki_parts()
+    void tebasaki_parts()
     {
         parts[18, 2] += 1;
         money -= parts[18, 1];
     }
-    public void tukune_parts()
+    void tukune_parts()
     {
         parts[19, 2] += 1;
         money -= parts[2, 1];
