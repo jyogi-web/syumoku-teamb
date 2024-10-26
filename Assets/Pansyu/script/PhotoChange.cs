b87ad555c77b279d0364fcd6f43928c0b4c679eb
pansyu <gutti1225yokono@gmail.com>
2024-10-27 03:59:55 +0900

    [add] 図鑑機能
    図鑑

---
+++ Assets/Pansyu/script/PhotoChange.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PhotoChange : MonoBehaviour
+{
+    private Image img;
+    private int count = 1;
+
+    void Start()
+    {
+        img = GameObject.Find("Image").GetComponent<Image>();
+    }
+
+    public void Next()
+    {
+        count++;
+        if (count >= 23)
+            count = 1;
+
+        img.sprite = Resources.Load<Sprite>("Image/photo" + count.ToString());
+    }
+
+    public void Back()
+    {
+        count--;
+        Debug.Log(count);
+        if (count <= 0)
+            count = 22;
+
+        img.sprite = Resources.Load<Sprite>("Image/photo" + count.ToString());
+    }
+}
