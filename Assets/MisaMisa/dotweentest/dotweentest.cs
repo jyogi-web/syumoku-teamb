ba193de774b3cbd5e796d15f2cc48296f7bb033b
MisamisaK <misamisa.k.06.05@gmail.com>
2024-10-26 16:14:15 +0900

    [add] gatya&appear

--- Assets/MisaMisa/dotweentest/dotweentest.cs
+++ Assets/MisaMisa/dotweentest/dotweentest.cs
@@ -14,23 +14,30 @@ public class dotweentest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        transform.DOLocalMove(new Vector3(210, 40, 0), 1f);
-        StartCoroutine("Flashing");
+        Tmp.text = "";
+        Tmp.color = new Color(0, 0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-       
+        
+    }
+    public void buttunpush()
+    {
+        transform.DOLocalMove(new Vector3(210, 40, 0), 1f);
+        StartCoroutine("Flashing");
     }
 
     IEnumerator Flashing()
     {
+        Tmp.text = "-50";
+        Tmp.color = Tmp.color+ new Color32(255, 224, 5, 255);
         while (true)
         {
-            Tmp.color = Tmp.color - new Color32(0, 0, 0, 2);
+            Tmp.color = Tmp.color - new Color32(0, 0, 0, 4);
 
-            yield return new WaitForSeconds(0.005f);
+            yield return new WaitForSeconds(0.01f);
 
             if (a_color < 0)
             {
